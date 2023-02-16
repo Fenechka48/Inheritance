90c7fea15a9e3642027c22fe4e8feed0aacd4d55
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-02-16 18:05:42 +0300

    class student

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,4 +1,4 @@
-//Academy
+﻿//Academy
 #include<iostream>
 #define _CRT_SECURE_NO_WARGNINGS
 using namespace std;
@@ -51,9 +51,8 @@ public:
 	{
 		set_last_name(last_name);
 		set_first_name(first_name);
-		set_birth_day(year,month,day);
-
-		cout << "HConstructor" << this << endl;
+		set_birth_date(year, month, day);
+		cout << "HConstructors:\t" << this << endl;
 	}
 		~Human()
 	{
@@ -68,22 +67,74 @@ public:
 };
 class Student : public Human
 {
-	std::string speciality;
+	std::string specialty;
 	std::string group;
 	double rating;
 	double attendance;
 public:
-	const std::string&& get_speciality ()const
+	const std::string& get_specialty()const
 	{
-		return speciality;
+		return specialty;
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	double get_rating()const
+	{
+		return rating;
+	}
+	double get_attendance()const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->specialty = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(double attendance)
+	{
+		this->attendance = attendance;
+	}
+
+	//						Constructors:
+	Student
+	(
+		const std::string& last_name, const std::string& first_name, int year, int month, int day,
+		const std::string& specialty, const std::string& group, double rating, double attendance
+	) :Human(last_name, first_name, year, month, day)
+	{
+		set_speciality(specialty);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SConstructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor:\t" << this << endl;
+	}
+	void info()const
+	{
+		Human::info();
+		cout << specialty << " " << group << " " << rating << " " << attendance << endl;
 	}
 };
+
 #define TIME_CHECK
 void main()
 {
 	setlocale (LC_ALL, "");
 #ifdef TIME_CHECK
-	time_t t_today = time(NULL);// �������� 
+	time_t t_today = time(NULL);// Получаем 
 	cout << t_today << endl;
 	tm* tm_today = localtime(&t_today);
 	cout << tm_today->tm_year << "\t";
@@ -91,5 +142,11 @@ void main()
 	cout << tm_today->tm_mday << "\t";
 	cout << asctime(tm_today) << "\t";
 #endif // TIME_CHECK
+	Human human("Тупенко", "Андрей", 1990, 04, 01);
+	human.info();
 
+	Student student("Тупенко", "Андрей", 1990, 04, 01, "IT", "start", 60, 30);
+	student.info();
+
+	student.get_birth_date();
 }
