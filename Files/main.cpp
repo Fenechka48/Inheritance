548b143652f060e46e2ddb371385a5d3269addc3
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-04-06 11:29:51 +0300

    HOME_DONE

--- Files/main.cpp
+++ Files/main.cpp
@@ -22,7 +22,7 @@ void main()
 	strcat_s(sz_command, _MAX_FNAME, filename);
 	system(sz_command);
 #endif // WRITE_TO_FILE
-#ifdef READ_TO_FILE
+#ifdef READ_FROM_FILE
 	ifstream fin;
 	fin.open("File.txt");
 	if (fin.is_open())
@@ -43,6 +43,6 @@ void main()
 		// cout-console out
 		//cerr- console error
 	}
-#endif // READ_TO_FILE
-
+#endif // READ_FROM_FILE
+	// //https://ru.wikipedia.org/wiki/%D0%92%D0%B5%D0%BD%D0%B3%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F_%D0%BD%D0%BE%D1%82%D0%B0%D1%86%D0%B8%D1%8F
 }
