cdda18ccefa7b59c4c4dce1d583c20c9d9c6e6cf
Tagun_py04@top-academy.ru <Tagun_py04@top-academy.ru>
2023-04-06 11:48:20 +0300

    Polymorphism

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -2,8 +2,34 @@
 #include<iostream>
 using namespace std;
 
+class Animal
+{
+public:
+	virtual void sound() = 0;
+};
+class Cat :public Animal
+{
+public:
+	void sound()
+	{
+		cout << "ּÿף" << endl;
+	}
+};
+class Dog :public Animal
+{
+public:
+	void sound()
+	{
+		cout << "ֳאג" << endl;
+	}
+};
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "ֿנטגוע" << endl;
+	Cat tom;
+	tom.sound();
+
+	Dog hatico;
+	hatico.sound();
 }
